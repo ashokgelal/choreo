b7daa63f8dc10700ab944af13727e7e53e0cfe85
Ashok Gelal <ashokgelal@gmail.com>
2023-07-13 16:43:29 -0400

    tests: ensure index only returns tasks owned by a user

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -6,10 +6,32 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Inertia\Testing\AssertableInertia as Assert;
 
 class TaskTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function index_only_returns_tasks_owned_by_user()
+    {
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+
+        $userTask1 = Task::factory()->create(['user_id' => $user->id]);
+        $userTask2 = Task::factory()->create(['user_id' => $user->id]);
+        $otherUserTask = Task::factory()->create(['user_id' => $otherUser->id]);
+
+        $this->actingAs($user)
+            ->get('/tasks')
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Tasks/Index')
+                ->has('tasks.data', 2)
+                ->where('tasks.data.0.id', $userTask1->id)
+                ->where('tasks.data.1.id', $userTask2->id)
+                ->missing('tasks.data.*.id', $otherUserTask->id)
+            );
+    }
 
     /** @test */
     public function subtask_cannot_be_added_for_subtasks()
